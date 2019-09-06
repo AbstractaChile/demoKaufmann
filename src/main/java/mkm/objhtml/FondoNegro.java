e1940f85bb47ac7c6e408a381b3339f2d8d4e959
AbstractaChile <ernesto.urbina@cl.abstracta.us>
2019-09-06 11:07:42 -0400

    cambiamos a notación el xpath de fondo negro

--- src/main/java/mkm/objhtml/FondoNegro.java
+++ src/main/java/mkm/objhtml/FondoNegro.java
@@ -1,15 +1,14 @@
 package mkm.objhtml;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.FindBy;
 
 public class FondoNegro extends PageObject {
 
-	private WebElement fondonegro = driver.findElement(By.xpath(
-			"/html[1]/body[1]/form[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]"));// *[@id="SECTIONFONDONEGRO"]
+	@FindBy(id = "SECTIONFONDONEGRO")
+	private WebElement fondonegro;
 
 	public FondoNegro(WebDriver driver) {
 		super(driver);
